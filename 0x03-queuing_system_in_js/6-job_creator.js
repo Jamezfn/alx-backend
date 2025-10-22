1cef107e4fd0c09dcc4701e51f37daec743a59fc
Jamezfn <jamesmakachia19@gmail.com>
2025-10-23 00:38:12 +0300

    6. Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,26 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+	phoneNumber: '4153518780',
+	message: 'This is a test notification',
+};
+
+const job = queue.create('push_notification_code', jobData);
+
+job.save((err) => {
+	if (!err) {
+		console.log('Notification job created: ' + job.id);
+	} else {
+		console.log('Notification job failed to be created: ' + err);
+	}
+});
+
+job.on('complete', () => {
+	console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+        console.log('Notification job failed');
+});
