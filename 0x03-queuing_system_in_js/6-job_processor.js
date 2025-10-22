046df023ef8573c4b55c052d397c100b3d82ba74
Jamezfn <jamesmakachia19@gmail.com>
2025-10-23 01:16:26 +0300

    8. Track progress and errors with Kue: Create the Job creator

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -9,4 +9,5 @@ function sendNotification(phoneNumber, message) {
 queue.process('push_notification_code', (job, done) => {
 	const { phoneNumber, message } = job.data;
 	sendNotification(phoneNumber, message);
+	done();
 });
